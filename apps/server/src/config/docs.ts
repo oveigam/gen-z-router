00b5067df1de40223d18a8b1848e4b06b9c5997f
oscar <o.veigam@gmail.com>
2023-03-19 23:14:57 +0100

    doc basic info

--- apps/server/src/config/docs.ts
+++ apps/server/src/config/docs.ts
@@ -5,24 +5,25 @@ extendZodWithOpenApi(z);
 
 export const openapi = new OpenAPIRegistry();
 
-export const openapiAuth = openapi.registerComponent("securitySchemes", "bearerAuth", {
-  type: "http",
-  scheme: "bearer",
-  bearerFormat: "username",
+export const openapiAuth = openapi.registerComponent("securitySchemes", "username", {
+  type: "apiKey",
+  name: "username",
+  description: "All requests must include the `username` header containing the employee code.",
+  in: "header",
 });
 
 export const generateDocs = () =>
   new OpenAPIGenerator(openapi.definitions, "3.0.0").generateDocument({
     info: {
       title: "Gen Z Validation",
-      description: "Mejor tipado, menos boilerplate.",
+      description: "Kneel before zod",
       contact: {
         name: "General Zod",
       },
-      license: {
-        name: "MIT",
-        url: "https://opensource.org/licenses/MIT",
-      },
       version: "1.0.0",
+    },
+    externalDocs: {
+      description: "Api Spec",
+      url: "/api-spec.json",
     },
   });
